0107d507d14c007516a8cf8ce360d3cd2315825c
robertpozun <robert.pozun@gmail.com>
2020-02-21 20:10:55 +0000

    amred1

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,13 +1,21 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+ 
 import org.json.simple.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 import java.io.IOException;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
 
 class TestClass {
     String authToken;
@@ -53,5 +61,61 @@ class TestClass {
         createBody.put("depositpaid", depositpaid);
         createBody.put("bookingdates", bookingdates);
         createBody.put("additionalneeds", additionalneeds);
+
+        Header h1 = new Header("Content-Type", "application/json");
+         Header h2 = new Header("Accept", "application/json");
+        Headers headers = new Headers(h1,h2);
+
+
+        
+        Response response = given().headers(headers).body(createBody.toJSONString()).when().post("/booking");
+       // Assertion assert = new Assertion();
+       // assert.assertEquals(response.getStatusCode(), 200)
+       System.out.println(response.getStatusCode());
+       JsonPath path = new JsonPath(response.getBody().asString());
+        System.out.println("he post response is" + response.getBody().asString());
+       System.out.println("The first name is: " + path.getString("booking.firstname"));
+      
+        String bookingid = path.getString("bookingid");
+
+        Response getresponse = given().headers(headers).when().get("/booking/"+bookingid);
+       // Assertion assert = new Assertion();
+        System.out.println("Get reponse status code" + getresponse.getStatusCode());
+        JsonPath path1 = new JsonPath(getresponse.getBody().asString());
+        System.out.println("the get response is" + getresponse.getBody().asString());
+         Assert.assertEquals(path1.getString("firstname"), firstname);
+
+        Header h3 = new Header("Content-Type", "application/json");
+         Header h4 = new Header("Accept", "application/json");
+        Header h5 = new Header("Cookie", "token="+authToken);
+        Headers headers1 = new Headers(h3,h4,h5);
+         
+        JSONObject createBody1 = new JSONObject();
+        createBody1.put("firstname", "James");
+    
+        Response putResponse = given().headers(headers1).body(createBody1.toJSONString()).when().patch("/booking/"+bookingid);
+        System.out.println("put reponse status code" + putResponse.getStatusCode());
+        JsonPath path2 = new JsonPath(putResponse.getBody().asString());
+        System.out.println("the put response is" + putResponse.getBody().asString());
+         Assert.assertEquals(path2.getString("firstname"), "James");
+
+
+         Response getresponse1 = given().headers(headers).when().get("/booking/"+bookingid);
+       // Assertion assert = new Assertion();
+        System.out.println("Get reponse1 status code" + getresponse1.getStatusCode());
+        JsonPath path3 = new JsonPath(getresponse1.getBody().asString());
+        System.out.println("the get response1 is" + getresponse1.getBody().asString());
+         Assert.assertEquals(path3.getString("firstname"), "James");
+
+
+
+
+        /* 
+            Create a booking using the above json object
+            Get the booking using the id returned and assert firstname == Robert
+            Update the firstname to James
+            Get the booking again and assert firstname == James
+        */
+
     }
 }
