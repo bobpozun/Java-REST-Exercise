9c3311fba02be66f9867ccc9687f4bb586c5ac97
robertpozun <robert.pozun@gmail.com>
2020-06-04 19:32:43 +0000

    rakesk1

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,10 +1,14 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import org.hamcrest.Matchers;
 import org.json.simple.JSONObject;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
+import io.restassured.response.*;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -53,5 +57,42 @@ class TestClass {
         createBody.put("depositpaid", depositpaid);
         createBody.put("bookingdates", bookingdates);
         createBody.put("additionalneeds", additionalneeds);
+
+        String createBookingJson= createBody.toJSONString();
+
+        // 1) Create a booking using the above json object
+        
+        Response response = given().log().all().header("Content-Type", "application/json")
+                .header("Accept","application/json")
+                .body(createBookingJson).when().
+                post("booking").then().log().all().assertThat().
+                statusCode(200).extract().response();
+        JsonPath path = response.jsonPath();
+
+        Integer id = path.getInt("bookingid");
+ 
+
+        // 2) Get the booking using the id returned and assert firstname == Robert
+        given().log().all().header("Accept", "application/json").pathParam("idparam", id)
+                .when()
+                .get("booking/{idparam}").
+                then().log().all().assertThat().statusCode(200).body("firstname", Matchers.equalTo("Robert"));
+                
+          
+
+        // 3) Update the firstname to James
+
+        JSONObject update= new JSONObject();
+        update.put("firstname","James");
+        String updateBookingJson= update.toJSONString();
+
+Response response2= given().log().all().header("Content-Type", "application/json").header("Accept",
+        "application/json").pathParam("idparam", id).body(updateBookingJson).
+         when().cookie("token", authToken).patch("booking/{idparam}").then().assertThat().statusCode(200).extract().response();
+ JsonPath path2 = response2.jsonPath();
+
+        // 4) Get the booking again and assert firstname == James
+ given().log().all().header("Accept", "application/json").pathParam("idparam", id).when().get("booking/{idparam}")
+         .then().log().all().assertThat().statusCode(200).body("firstname", Matchers.equalTo("James"));
     }
 }
